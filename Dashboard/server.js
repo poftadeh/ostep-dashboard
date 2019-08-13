7a027a02e5e3ab713163fe3b47d1305687042cd3
Pouya Oftadeh <poftadeh2@gmail.com>
2019-08-13 19:42:56 -0400

    fix: chaining of ipfilter

--- Dashboard/server.js
+++ Dashboard/server.js
@@ -7,28 +7,28 @@ const { whitelist, users } = require('../config-files/authentication');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
 const ipResultCheck = (err, req, res, next) => {
-  if (err) {
-    err instanceof IpDeniedError ? next() : next(err);
-  } else {
+  if (err instanceof IpDeniedError) {
     next();
+  } else {
+    next(err);
   }
 };
 
 //app.use('/', IpFilter(whitelist, { mode: 'allow' }));
+
+app.use(
+  IpFilter(whitelist, { mode: 'allow' }),
+  ipResultCheck,
+  basicAuth({ challenge: true, users })
+);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.get(
-  '/test',
-  IpFilter(whitelist, { mode: 'allow' }),
-  ipResultCheck,
-  basicAuth({ challenge: true, users })
-);
 
 app.get('/data/:containerName', (req, res, next) => {
   request(`http://${req.params.containerName}`, (error, _response, body) => {
@@ -43,15 +43,8 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   console.error('Error handler:', err);
-  console.log('err status', err.status);
-  if (err instanceof IpDeniedError) {
-    next();
-  } else {
-    res.status(err.status || 500).json({ message: err.message });
-  }
+  res.status(err.status || 500).json({ message: err.message });
 });
-
-//app.use(basicAuth({ challenge: true, users }));
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Dashboard container is listening on port ${PORT}`);
